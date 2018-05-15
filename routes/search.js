0e28ebe4e5cd98b3a84b2efde235c03b1f764bfb
Christian Vaagan <cvagan@gmail.com>
2018-05-15 14:43:05 +0200

    add and test node-fetch for ES6 refactor

--- routes/search.js
+++ routes/search.js
@@ -4,16 +4,18 @@ var request = require("request");
 var bodyParser = require("body-parser");
 var middleware = require("../middleware");
 var User = require("../models/user.js");
+var fetch = require("node-fetch");
 
 router.get("/search", function(req,res) {
     var searchTerm = "http://www.omdbapi.com/?s=" + req.query.title + "&apikey=" + process.env.APIKEY;
-    request(searchTerm, function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-            var data = JSON.parse(body);
-            res.render("search/result", {dataObj: data});
-        }
-        else console.log(error);
-    });
+    // request(searchTerm, function(error, response, body) {
+    //     if (!error && response.statusCode === 200) {
+    //         var data = JSON.parse(body);
+    //         res.render("search/result", {dataObj: data});
+    //     }
+    //     else console.log(error);
+    // });
+    fetch(searchTerm).then(result => result.json()).then(data => res.render("search/result", {dataObj: data})).catch(err => console.log(err));
 });
 
 router.get("/search/:id", function(req,res) {
